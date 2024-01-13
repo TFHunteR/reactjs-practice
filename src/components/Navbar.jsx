11578aa2cc4219f98aff140ab1d34dfa6594233f
TFHunteR <gatchalianjohnpaul101901@gmmail.com>
2024-01-13 15:30:05 +0800

    added channel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import {NavLink, Link} from 'react-router-dom'
 import './Navbar.css'
 export const Navbar = () => {
+    const navBackground = {
+        backgroundColor:'#0F1035'
+    }
   return (
     <>
-    <nav>
-        <Link to='/home' className='title'>Website</Link>
-        <ul>
-            <li>
-                <NavLink to='/about'>About</NavLink>
+    <nav style={navBackground} className='d-flex justify-content-between align-items-center m-0 px-5'>
+        <Link to='/home' className='fs-3 ml-5 text-decoration-none fw-bold p-3 text-white'>Website</Link>
+        <ul className='m-0 p-0 d-flex '>
+            <li  className='nav-item'>
+                <NavLink to='/about'className='d-block text-decoration-none rounded text-white mx-2 p-2 fw-bold'>About</NavLink>
             </li>
-            <li>
-                <NavLink to='/Services'>Services</NavLink>
+            <li className='nav-item'>
+                <NavLink to='/Services' className='d-block text-decoration-none rounded text-white mx-2 p-2 fw-bold'>Services</NavLink>
             </li>
-            <li>
-                <NavLink to='/Contact'>Contact</NavLink>
+            <li className='nav-item'>
+                <NavLink to='/Contact'  className='d-block text-decoration-none rounded text-white mx-2 p-2 fw-bold'>Contact</NavLink>
             </li>
         </ul>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export const Home = () => {
+  const cardBody = {margin:'2rem'}
   return (
     <>
-    <h1>Home</h1>
-    <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iste eum doloremque labore dignissimos placeat eveniet dolores voluptatem quae, iusto consequuntur magni aliquid facere impedit debitis fugiat suscipit ab natus illum quis corporis deserunt. Alias dolor magni, quae facilis fuga numquam dignissimos, sed ducimus, deserunt harum iste. Delectus, illo commodi?
+    <div class="card"style={cardBody}>
+      <div class="card-header">
+        Featured
+      </div>
+      <div class="card-body">
+        <h5 class="card-title fw-bold">HOME</h5>
+        <p class="card-text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum consectetur molestiae dolores quis ut corporis explicabo fugit tempora, excepturi, porro amet magni saepe, assumenda hic repudiandae similique nemo repellat necessitatibus ipsa unde recusandae voluptatibus qui. Saepe impedit ratione esse ipsum, porro quis fugit dignissimos cupiditate, reprehenderit sit cumque illo. Nulla magnam eveniet obcaecati in voluptate? Amet distinctio pariatur iusto maxime nisi minima nihil tempora quos quis sint fuga, possimus mollitia asperiores voluptatibus necessitatibus repellat enim, iure rerum aperiam veritatis eveniet. Maiores, amet reprehenderit. Odit quisquam, fugiat voluptas, sed at, dolorum cupiditate dolores deserunt molestiae maxime repudiandae consequatur? Totam sapiente animi fugit ducimus commodi iste et cum consequatur maxime pariatur ratione, distinctio aspernatur iure eos natus non, dolorum dicta? Laboriosam accusamus nihil, corrupti quibusdam sapiente aut vero illum repellat sit magnam reiciendis! Veritatis, quaerat nemo quis quos dolore mollitia facilis cumque laborum aut, possimus, aperiam asperiores reiciendis dolorum nam ipsum eius quam necessitatibus natus! Neque accusamus, mollitia sapiente, provident voluptates molestias obcaecati blanditiis quia inventore qui aliquam ducimus tempore optio recusandae, soluta sunt nihil velit aliquid quidem ipsa debitis laborum. Autem voluptate quam incidunt esse aspernatur alias. Omnis nostrum vel voluptatibus iure rerum illum magnam delectus quas. Aliquam magni porro, optio expedita odio saepe placeat distinctio, aut modi consequuntur sint, rerum quis labore nemo natus assumenda commodi alias eum. Est architecto quis quam odit. Fugit corporis reprehenderit necessitatibus quod omnis sapiente molestiae veniam dolor quidem! Totam quis reprehenderit nostrum, laboriosam nisi esse recusandae illum doloremque ipsum exercitationem doloribus culpa omnis, harum iure reiciendis nemo rem fuga quidem sunt dolore dolorem eveniet eaque velit. Reiciendis et dicta adipisci commodi minus neque ipsum debitis fugit quas, magnam veritatis ex, voluptas iusto non pariatur ratione rerum. Consequuntur quae, asperiores voluptates maxime numquam recusandae dignissimos amet? Excepturi quam alias quo ipsam minus sequi recusandae laborum officia quos quis aliquid obcaecati dignissimos voluptates, rerum provident dolorum deleniti. Et praesentium unde autem eius quis suscipit, odio impedit possimus eveniet maiores aliquam velit pariatur illo debitis similique ex asperiores facilis, dicta reprehenderit, quia voluptas! Soluta quidem, fugit obcaecati voluptatum in, reiciendis sunt, perferendis perspiciatis error possimus labore! Sequi, unde. Autem temporibus hic, aut enim doloribus et corrupti perspiciatis maxime placeat ea quas vel veritatis eligendi quo explicabo laudantium cumque. Quisquam, soluta ex ipsa tenetur sunt nobis sequi voluptas dolorum quam perferendis ipsum dolorem earum ab modi corporis eius vitae quas. Accusamus, expedita. Perferendis delectus quos rem veritatis numquam ex quaerat eligendi, commodi illo culpa? Reiciendis maxime iure deserunt doloremque, eligendi, est vitae minima quidem officia fugiat, ea praesentium. Perferendis, ab! Dignissimos ducimus maxime doloribus facere suscipit impedit ex. Hic optio aut eius laudantium rem, sint voluptate odit. Necessitatibus illo repudiandae nam autem sequi nihil porro. Dolorem et sed odit eaque expedita, fuga velit quod maxime similique voluptates consequuntur consequatur. Eveniet iure saepe minus, laudantium atque asperiores expedita voluptatem amet illum aut exercitationem porro debitis ullam aliquam ad? Quos in minima quasi. Odit fugit saepe consequatur illo dolorum ad quibusdam cumque libero excepturi aspernatur. Quasi eos sit nesciunt magnam?
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
     </>
     
